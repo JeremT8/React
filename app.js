82ac063004ecad06c530edde0aea50ef5987ac10
JeremT8 <jeremytilletpro@gmail.com>
2021-09-08 18:17:35 +0200

    discover form with React

--- app.js
+++ app.js
@@ -1,114 +1,42 @@
-function WelcomeFunc ({name, children}) {
+class Home extends React.Component {
 
-	return <div>
-		<h1> Bonjour {name} </h1>
-		<p>
-			{children}
-		</p>
-	</div>
-}
+      constructor (props) {
+            super(props)
+            this.state = {
+                  nom: "",
+                  prenom: "",
+                  newsletter: false
+            }
+            this.handleChange = this.handleChange.bind(this)
+      }
 
-class Welcome extends React.Component {
-
-	render () {
-		return <div>
-			<h1>Bonjour {this.props.name}</h1>
-			<p>
-				{this.props.children}
-			</p>
-			</div>
-	}
-}
-
-class Clock extends React.Component {
-
-	constructor (props) {
-		super(props)
-		this.state = {date: new Date()}
-		this.timer = null
-	}
-
-	componentDidMount () {
-		this.timer = window.setInterval (this.tick.bind(this), 1000)
-	}
-
-	componentWillUnmount () {
-		window.clearInterval(this.timer)
-	}
-
-	tick () {
-		this.setState({date: new Date()})
-	}
-
-	render () {
-		const date = new Date()
-		return <div>
-			Il est {this.state.date.toLocaleDateString()} {this.state.date.toLocaleTimeString()}
-		</div>
-	}
-}
-
-class Incrementer extends React.Component {
-
-	constructor (props) {
-		super(props)
-		this.state = {n: props.start, timer: null}
-		this.timer = null
-	}
-
-	componentDidMount () {
-		this.play()
-	}
-
-	componentWillUnmount () {
-            window.clearInterval(this.state.timer)
-	}
-
-	increment () {
-		this.setState(function (state, props) {
-		return {n: state.n + 1}	
-		})
-	}
-
-      pause () {
-            window.clearInterval(this.state.timer)
+      handleChange (e) {
+            
+            const name = e.target.name
+            const type = e.target.type
+            const value = type === 'checkbox' ? e.target.checked : e.target.value
             this.setState({
-                  timer: null
+                       [name]: value
             })
       }
 
-      play () {
-            window.clearInterval(this.state.timer)
-            this.setState({
-            timer: window.setInterval(this.increment.bind(this), 1000)
-      })
-      }
-
-	render () {
-		return <div> valeur : {this.state.n}
-            {this.state.timer ?
-            <button onClick={this.pause.bind(this)}>Pause</button> :
-            <button onClick={this.play.bind(this)}>Lecture</button>
-            }
+      render() {
+            return <div>
+                  <div>
+                        <label htmlFor="nom">Nom</label>
+                        <input type="text" value={this.state.nom} onChange={this.handleChange} id="nom" name="nom"></input>
+                  </div>
+                  <div>
+                        <label htmlFor="prenom">Prénom</label>
+                        <input type="text" value={this.state.prenom} onChange={this.handleChange} id="prenom" name="prenom"></input>
+                  </div>
+                  <div>
+                        <label htmlFor="newletter">S'abonner à la newsletter</label>
+                        <input type="checkbox" checked={this.state.newsletter} onChange={this.handleChange} id="newsletter" name="newsletter"></input>
+                  </div>
+                  {JSON.stringify(this.state)}
             </div>
-	}
+      }
 }
-
-
-Incrementer.defaultProps = {
-      start: 0,
-      step: 1
-}
-
-function Home () {
-	return <div>
-		<Welcome name =  "Dorothée" />
-		<Welcome name =  "Jean" />
-		<Incrementer/>
-	</div>
-}
-
-
-
 
 ReactDOM.render(<Home/>, document.querySelector('#app'))
